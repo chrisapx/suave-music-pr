6b27cb8e6bc35ff34aab432ce5826ff5fedeca94
chrisapx <mcaplexyc@gmail.com>
2025-02-21 17:55:22 +0300

    Add logos for as senn as

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
   const [toggleMusicForm, setToggleMusicForm] = useState(false);
   const team = [
     { name: 'Joe Watema', image: 'images/Joseph Watema pic.png', title: 'Team Lead', description: 'joe.watema@suavemusicpr.com' },
-    { name: 'Tela Wangeci', image: 'images/Tela Wangeci pic.jpg', title: 'Ops & Partnerships', description: '--' },
+    { name: 'Tela Wangeci', image: 'images/Tela Wangeci pic.jpg', title: 'Ops & Partnerships', description: 'tela@suavemusicpr.com' },
     { name: 'Christopher Mwesigwa', image: '', title: 'IT & Systems', description: 'admin@suavemusicpr.com' }
   ];
 
@@ -17,6 +17,7 @@ const AboutUs = () => {
 
   const logos = [
     'logos/mugibson.com logo.png',
+    'images/The Culturing logo.png',
   ];  
 
   return (
@@ -80,7 +81,7 @@ const AboutUs = () => {
       <section className='mb-16'>
         <h1 className="text-center text-3xl font-bold py-8">As Seen In</h1>
 
-        <div className='flex justify-center items-center w-full'>
+        {/* <div className='flex justify-center items-center w-full'>
           <div className="relative w-[300px] h-[300px] border border-gray-300 rounded-lg shadow-xl overflow-hidden">
             <img
               src={'images/The Culturing logo.png' || "/svgs/logo.svg"}
@@ -91,21 +92,21 @@ const AboutUs = () => {
               <h2 className="text-lg font-semibold">The Culturing</h2>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* <h1 className="text-center text-3xl font-bold mt-16 py-2 border-y">More Partners</h1> */}
-        {/* <div className="py-10 overflow-hidden text-center">
-          <div className="flex gap-24">
+        <div className="py-10 overflow-hidden text-center border-t">
+          <div className="flex justify-center gap-24">
             {logos.map((logo, index) => (
               <img
                 key={index}
                 src={logo}
                 alt={`Logo ${index + 1}`}
-                className="h-8 w-auto object-contain"
+                className="h-12 w-auto object-contain"
               />
             ))}
           </div>
-        </div> */}
+        </div>
       </section>
 
       <div className="md:absolute w-full">
